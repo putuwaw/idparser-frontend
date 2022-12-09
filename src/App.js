6c8b7a060e4be7e7494c511fff58f9e3c13f6b30
Putu Widyantara <putuwaw973@gmail.com>
2022-12-09 23:27:25 +0800

    Create simple app to check palindrome string

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Col , Row , Form, Button} from 'react-bootstrap' ;
+import { useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    const [input , setInput] = useState('');
+    const [result, setResult] = useState('');
+    const [data , setData] = useState('');
+    const [isPost , setPost] = useState(false);
+
+    const palindromeCheck = async(e) => {
+        e.preventDefault();
+        const Response = await axios.post(process.env.REACT_APP_API_PATH_TEST , 
+        { string : input });
+        setData(Response.data.result);
+        setPost(true);
+        setResult(input);
+    }
+    
+    return(
+        <Container>
+            <Row>
+                <Col lg = {6}>
+                    <Form>
+                        <Form.Group controllid = "App">
+                            <Form.Label><h5>String</h5></Form.Label>
+                            <Form.Control type = "text" placeholder = "Masukkan string..." onChange = {(e) => setInput(e.target.value)} />
+                        </Form.Group>
+                        <Button type = 'submit' className = 'px-4 py-2 mt-3' onClick = {(e) => palindromeCheck(e)}> Periksa </Button>
+                    </Form>
+                    {isPost &&
+                        <div className='mt-3'>
+                            <h5>Hasil</h5>
+                            <p>String <code>{ result }</code> adalah { data }</p>
+                        </div>
+                    }
+                </Col>
+            </Row>
+        </Container>
+    )
 }
-
-export default App;

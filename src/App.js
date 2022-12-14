99f679102ce488ad48ab75f7dc56027d7ad8ece9
Putu Widyantara <putuwaw973@gmail.com>
2022-12-14 23:51:49 +0800

    Display simple graph when input is valid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { Container, Col , Row , Form, Button} from 'react-bootstrap' ;
+import { Container, Col, Row, Form, Button} from 'react-bootstrap' ;
 import { useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Graphviz } from 'graphviz-react';
 
 export default function App(){
-    const [input , setInput] = useState('');
+    const [input, setInput] = useState('');
     const [result, setResult] = useState('');
-    const [data , setData] = useState('');
-    const [isPost , setPost] = useState(false);
+    const [data, setData] = useState('');
+    const [graph, setGraph] = useState('');
+    const [isPost, setPost] = useState(false);
 
     const palindromeCheck = async(e) => {
         e.preventDefault();
         const Response = await axios.post(process.env.REACT_APP_API_PATH_TEST , 
         { string : input });
         setData(Response.data.result);
+        setGraph(Response.data.graph);
         setPost(true);
         setResult(input);
     }
-    
+
     return(
         <Container>
             <Row>
@@ -29,10 +32,12 @@ export default function App(){
                         </Form.Group>
                         <Button type = 'submit' className = 'px-4 py-2 mt-3' onClick = {(e) => palindromeCheck(e)}> Periksa </Button>
                     </Form>
+                    <div className='graph'></div>
                     {isPost &&
                         <div className='mt-3'>
                             <h5>Hasil</h5>
                             <p>String <code>{ result }</code> adalah { data }</p>
+                            <Graphviz options={{height: 300, width: 300}} dot={graph} />
                         </div>
                     }
                 </Col>

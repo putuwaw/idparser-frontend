9f836f63efa173e426a1065a3d4e9d9011159490
Putu Widyantara <putuwaw973@gmail.com>
2022-12-17 02:38:07 +0800

    Change API endpoint for parsing sentence

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import { Container, Col, Row, Form, Button} from 'react-bootstrap' ;
 import { useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Graphviz } from 'graphviz-react';
+// import { Graphviz } from 'graphviz-react';
 
 export default function App(){
     const [input, setInput] = useState('');
     const [result, setResult] = useState('');
     const [data, setData] = useState('');
-    const [graph, setGraph] = useState('');
+    // const [graph, setGraph] = useState('');
     const [isPost, setPost] = useState(false);
 
     const palindromeCheck = async(e) => {
         e.preventDefault();
-        const Response = await axios.post(process.env.REACT_APP_API_PATH_TEST , 
+        const Response = await axios.post(process.env.REACT_APP_API_PATH_PARSER , 
         { string : input });
         setData(Response.data.result);
-        setGraph(Response.data.graph);
+        // setGraph(Response.data.graph);
         setPost(true);
         setResult(input);
     }
@@ -28,7 +28,7 @@ export default function App(){
                     <Form>
                         <Form.Group controllid = "App">
                             <Form.Label><h5>String</h5></Form.Label>
-                            <Form.Control type = "text" placeholder = "Masukkan string..." onChange = {(e) => setInput(e.target.value)} />
+                            <Form.Control type = "text" placeholder = "Masukkan kalimat..." onChange = {(e) => setInput(e.target.value)} />
                         </Form.Group>
                         <Button type = 'submit' className = 'px-4 py-2 mt-3' onClick = {(e) => palindromeCheck(e)}> Periksa </Button>
                     </Form>
@@ -36,10 +36,10 @@ export default function App(){
                     {isPost &&
                         <div className='mt-3'>
                             <h5>Hasil</h5>
-                            <p>String <code>{ result }</code> adalah { data }</p>
-                            {data === 'String palindrome' &&
+                            <p>Kalimat <code>{ result }</code> adalah { data }</p>
+                            {/* {data === 'String palindrome' &&
                                 <Graphviz options={{height: 300, width: 300}} dot={graph} />
-                            }
+                            } */}
                         </div>
                     }
                 </Col>

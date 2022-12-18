810f35c71b64b263f94760808b7e1e00b1e08366
Putu Widyantara <putuwaw973@gmail.com>
2022-12-18 20:06:22 +0800

    Create footer components

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+
+function Footer() {
+  return (
+    <>
+    <div className="bg-blue-500 font-poppins">
+      <footer className="flex items-center justify-center h-14 text-white font-semibold">Copyright &copy; idParser 2022</footer>
+    </div>
+    </>
+  );
+}
+
+export default Footer;

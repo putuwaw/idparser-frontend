09db9476248a6d87a8b192b5b67c71330011a5fa
Putu Widyantara <putuwaw973@gmail.com>
2022-12-18 20:07:30 +0800

    Update unit testing with new components

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders check button', () => {
   render(<App />);
-  const checkButton = screen.getByText("Periksa");
+  const checkButton = screen.getByText("Selengkapnya");
   expect(checkButton).toBeTruthy();
 });
